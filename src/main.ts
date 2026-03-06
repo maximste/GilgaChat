4aeeffc9501d4e331bb8a4c9ad403809061d52f5
Maxim Steinbrecher <maximste@gmail.com>
2026-03-06 23:07:25 +0300

    Реализован рендер компонента App

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { AuthForm } from './pages/Auth/AuthForm'
+import './style.scss'
+
+class App {
+  private authForm: AuthForm | null = null;
+  constructor() {
+    this.init();
+  }
+
+  private init(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      // TODO временная заглушка, пока не реализован лейаут
+      this.renderAuthForm();
+    })
+  }
+
+  private renderAuthForm(): void {
+    const container = document.getElementById('app');
+
+    if(!container) {
+      console.error('App container not found');
+      return
+    }
+
+    this.authForm = new AuthForm(container, {
+      title: 'Welcome back',
+    })
+
+    this.authForm.render();
+  }
+}
+
+new App()
