a25ba69f38fb201b6d30f2e68251bd590552f5f5
Maxim Steinbrecher <maximste@gmail.com>
2026-03-07 15:51:58 +0300

    Добавлены типы для Link

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import type { InputProps, InputType } from './InputTypes';
 import type { LabelProps } from './LabelTypes';
+import type { LinkProps } from './LinkTypes';
 
-export type { InputProps, InputType, LabelProps };
+export type { InputProps, InputType, LabelProps, LinkProps };
