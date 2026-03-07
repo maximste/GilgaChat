d1450e5bcd4746042dbc6e74acac357b537bbe9c
Maxim Steinbrecher <maximste@gmail.com>
2026-03-07 15:26:51 +0300

    Добавлен компонент Input

---
+++ src/components/Input/Input.ts
@@ -0,0 +1,23 @@
+import Handlebars from 'handlebars';
+import template from './Input.hbs?raw';
+import type { InputProps } from '../../types';
+
+import './Input.scss';
+
+class Input {
+  private props: InputProps;
+  
+  constructor(props: InputProps){
+    this.props = props;
+  }
+
+  public getData(): InputProps {
+    return this.props;
+  }
+
+  public render(props: InputProps): string {
+    return Handlebars.compile(template)(props);
+  }
+};
+
+export { Input };
