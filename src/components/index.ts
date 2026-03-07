352291bd38807373ef4aa40abcc2730f18c2ace1
Maxim Steinbrecher <maximste@gmail.com>
2026-03-07 15:35:03 +0300

    Добавлен реэкспорт из папки components

---
+++ src/components/index.ts
@@ -0,0 +1,20 @@
+import { Button } from './Button/Button';
+import { FormField } from './FormField/FormField';
+import { Input } from './Input/Input';
+import { Label } from './Label/Label';
+
+import ButtonTemplate from './Button/Button.hbs?raw';
+import InputTemplate from './Input/Input.hbs?raw';
+import LabelTemplate from './Label/Label.hbs?raw';
+import FormFieldTemplate from './FormField/FormField.hbs?raw';
+
+export {
+  Button,
+  ButtonTemplate,
+  FormField,
+  FormFieldTemplate,
+  Input, 
+  InputTemplate,
+  Label,
+  LabelTemplate
+};
