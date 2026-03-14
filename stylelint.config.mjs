a759af90107ecf7f45882b4830963fca12344ac2
Maxim Steinbrecher <maximste@gmail.com>
2026-03-14 21:46:23 +0300

    Добавлена конфигурация Stylelint

---
+++ stylelint.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-order"],
+  rules: {
+    "order/properties-alphabetical-order": true,
+  },
+};
