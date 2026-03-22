b6209a4b8e70f30af2a49cf1528ec011a36b8874
Maxim Steinbrecher <maximste@gmail.com>
2026-03-22 23:29:20 +0300

    Обновлена боковая панель:

--- src/widgets/sidebar/Sidebar.ts
+++ src/widgets/sidebar/Sidebar.ts
@@ -2,30 +2,14 @@ import type { LinkProps } from "@/shared/lib/types";
 import { Block, type BlockOwnProps } from "@/shared/ui/block";
 
 import template from "./Sidebar.hbs?raw";
+import type { DirectMessageItem, GroupItem, SidebarCurrentUser } from "./types";
 
 import "./Sidebar.scss";
-
-export interface DirectMessageItem {
-  firstName: string;
-  lastName: string;
-  preview: string;
-  statusType: "green" | "yellow" | "gray";
-}
-
-export interface GroupItem {
-  name: string;
-  preview: string;
-  iconClass: string;
-}
 
 export interface SidebarProps {
   appTitle: string;
   topLinks: LinkProps[];
-  currentUser: {
-    firstName: string;
-    lastName: string;
-    status: string;
-  };
+  currentUser: SidebarCurrentUser;
   directMessages: DirectMessageItem[];
   groups: GroupItem[];
   /** HTML-фрагмент справа от текста (иконка и т.п.) */
--- src/widgets/sidebar/index.ts
+++ src/widgets/sidebar/index.ts
@@ -1,8 +1,21 @@
 export {
-  type DirectMessageItem,
-  type GroupItem,
   Sidebar,
   SIDEBAR_SELECT_CHAT_EVENT,
   type SidebarProps,
   type SidebarSelectChatDetail,
 } from "./Sidebar";
+export {
+  SidebarChatListItem,
+  type SidebarChatListItemProps,
+} from "./SidebarChatListItem";
+export {
+  SidebarChatSection,
+  type SidebarChatSectionKind,
+  type SidebarChatSectionProps,
+} from "./SidebarChatSection";
+export { SidebarHeader, type SidebarHeaderProps } from "./SidebarHeader";
+export {
+  SidebarUserPanel,
+  type SidebarUserPanelProps,
+} from "./SidebarUserPanel";
+export type { DirectMessageItem, GroupItem, SidebarCurrentUser } from "./types";
