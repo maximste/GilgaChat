7170d0924339228de83f2fbf631a419bde686f19
Maxim Steinbrecher <maximste@gmail.com>
2026-03-22 22:04:26 +0300

    Добавлена регистрация компонентов IconButton и Textarea

--- src/shared/ui/block/registerBlocks.ts
+++ src/shared/ui/block/registerBlocks.ts
@@ -1,12 +1,16 @@
 import { Button } from "../button/Button";
 import { FormField } from "../formField/FormField";
+import { IconButton } from "../iconButton/IconButton";
 import { Input } from "../input/Input";
 import { Label } from "../label/Label";
 import { Link } from "../link/Link";
+import { Textarea } from "../textarea/Textarea";
 import { registerComponent } from "./lib/registerComponent";
 
 registerComponent(Button);
+registerComponent(IconButton);
 registerComponent(Input);
 registerComponent(Label);
 registerComponent(Link);
 registerComponent(FormField);
+registerComponent(Textarea);
